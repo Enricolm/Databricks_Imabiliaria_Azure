c72407df915c182027fb968056a89d714a6e3647
Enricolm <Enricolm>
2023-10-19 02:40:43 +0000

    terminei_a_bronze

---
+++ Notebooks/Extraction_Bronze.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+import pyspark.sql.functions as f
+
+# COMMAND ----------
+
+dados_raw = spark.read.json("/mnt/data/Raw/")
+dados_without_col = dados_raw.drop("imagens", "usuario")
+display(dados_without_col)
+
+# COMMAND ----------
+
+dados_without_col.printSchema()
+
+# COMMAND ----------
+
+dados_format = dados_without_col.withColumn("id", f.col("anuncio.id"))
+display(dados_format)
+
+# COMMAND ----------
+
+dados_format.write.format("parquet")\
+    .mode("overwrite")\
+    .save("/mnt/data/Bronze/")
+
